bbcb055d74aabd016238d871857649282a5a7e79
Song Phattranit <songsuay@songs-macbook-pro.local>
2018-11-07 00:03:27 +0700

    Initial Commit

---
+++ miniproject_13600197/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniproject_13600197
+//
+//  Created by songsuay on 11/7/18.
+//  Copyright © 2018 ICTSUIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
